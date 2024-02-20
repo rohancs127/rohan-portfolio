ce1da81d12e0914fbdb78e7b4fc27e0e4d885ef3
Rohan <rohankumar5803@gmail.com>
2024-02-20 22:08:27 +0530

    removed app.css styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <div className='text-5xl text-blue-300'>Hello World!</div>
-    </>
+    <Navbar/>
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = ()=> {
+  return (
+    <div className='text-red'>
+    Hello
+    </div>
+  )
+}
+
+export default Navbar
