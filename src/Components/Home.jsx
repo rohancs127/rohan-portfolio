11ec858685bf2a4cb2c10078c26a0638afbde8d1
Rohan <rohankumar5803@gmail.com>
2024-02-23 22:31:50 +0530

    updated position

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Typewriter from 'typewriter-effect/dist/core';
+
+// new Typewriter('#bio-text', {
+//   strings: ["I'm a Computer Science Engineer.."],
+//   autoStart: true,
+// });
 
 function Home() {
   return (
-    <div className='flex w-full mt-20 p-20 justify-center gap-52'>
+    <div className='flex w-full mt-20 mr-24 p-20 justify-center gap-60'>
         <div>
            <img src='images/Rohan.jpg' width={300} className='rounded-full'/> 
         </div>
-        <div className='flex-2 pt-10'>
-            <h1>Hi</h1>
-            <h2>I am Rohan</h2>
-            <h3>I'm a Computer Science Engineer..</h3>
+        <div className='flex-col pt-10'>
+            <h1 className='mb-5 font-bold text-yellow-200 text-7xl'>Hi</h1>
+            <h2 className='mb-5 font-bold text-5xl'>I am Rohan</h2>
+            <h3 id='bio-text' className='text-4xl font-mono'>I'm a Computer Science Engineer..</h3>
         </div>
     </div>
   )
