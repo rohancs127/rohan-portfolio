e21dd979472b4c6026670024375436cb804022ef
Rohan <rohankumar5803@gmail.com>
2024-02-23 21:09:06 +0530

    positioned home page contents

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='flex w-full mt-20 gap-20 p-20 align-middle'>
+    <div className='flex w-full mt-20 p-20 justify-center gap-52'>
         <div>
-           <img src='images/Rohan.jpg' className=''/> 
+           <img src='images/Rohan.jpg' width={300} className='rounded-full'/> 
         </div>
-        <div className='w-full'>
+        <div className='flex-2 pt-10'>
             <h1>Hi</h1>
             <h2>I am Rohan</h2>
+            <h3>I'm a Computer Science Engineer..</h3>
         </div>
     </div>
   )
