1d8f0b8d09dafa04461567cec73eba93e93e5b7c
Rohan <rohankumar5803@gmail.com>
2024-03-01 22:21:33 +0530

    added skill card

---
+++ src/Components/Skill.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Skill() {
+  return (
+    <div className='flex gap-5 items-center p-2 px-6 m-7 w-max bg-gray-950 rounded-md hover:shadow-gray-700 shadow transition-all duration-300'>
+        <img src='../../src/assets/skillLogos/c++.svg' className='w-14'/>
+        <div>C++</div>
+    </div>
+  )
+}
+
+export default Skill
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
-import Typewriter from 'typewriter-effect/dist/core';
-
-// new Typewriter('#bio-text', {
-//   strings: ["I'm a Computer Science Engineer.."],
-//   autoStart: true,
-// });
+import Skill from '../Components/Skill';
 
 function Home() {
   return (
-    <div className='flex w-full mt-20 mr-24 p-20 justify-center gap-60'>
+    <div>
+        <div className='flex w-full mt-32 mr-24 p-20 justify-center gap-60'>
         <div>
            <img src='images/Rohan.jpg' width={300} className='rounded-full'/> 
         </div>
         <div className='flex-col pt-10'>
             <h1 className='mb-5 font-bold text-yellow-200 text-7xl'>Hi</h1>
             <h2 className='mb-5 font-bold text-5xl'>I am Rohan</h2>
-            <h3 id='bio-text' className='text-4xl font-mono'>I'm a Computer Science Engineer..</h3>
+            <h3 className='text-4xl font-mono'>I'm a Computer Science Engineer..</h3>
         </div>
+      </div>
+      <div className='flex flex-wrap justify-center'>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+        <Skill/>
+      </div>
     </div>
   )
 }
