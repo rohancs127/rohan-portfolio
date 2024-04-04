3d3e7c2705486b95fd8aa94f6be68da0ef38566a
Rohan <rohankumar5803@gmail.com>
2024-04-04 20:57:55 +0530

    adss heading for skills section

--- src/Pages/SkillSection.jsx
+++ src/Pages/SkillSection.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import Skill from '../Components/Skill';
+import React from "react";
+import Skill from "../Components/Skill";
 
 function SkillSection() {
   return (
-    <div className="flex flex-wrap justify-center">
+    <div>
+      <div className="flex justify-center p-10">
+        <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300">
+          Skills
+        </h1>
+      </div>
+      <div className="flex flex-wrap justify-center">
         <Skill image="../../src/assets/skillLogos/c++.svg" skill="C++" />
         <Skill image="../../src/assets/skillLogos/java.svg" skill="JAVA" />
         <Skill image="../../src/assets/skillLogos/python.svg" skill="PYTHON" />
@@ -20,7 +26,8 @@ function SkillSection() {
           skill="TAILWIND CSS"
         />
       </div>
-  )
+    </div>
+  );
 }
 
-export default SkillSection
+export default SkillSection;
