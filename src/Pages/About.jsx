16b1587cc67816a97ef7d9b97283fe2e8075722e
Rohan <rohankumar5803@gmail.com>
2024-04-05 22:05:58 +0530

    adds temporary content for about section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <div>About</div>
+    <div><p>My name is Rohan. I'm pursuing my Bachelor of Engineering degree in Computer Science and Engineering. I'm interested in the field of Web development. I have a good understanding in programming languages.</p></div>
+  </div>;
 }
 
 export default About;
