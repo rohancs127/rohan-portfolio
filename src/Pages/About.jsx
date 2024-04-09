3c4dd3248f811a67079100a58c9590f9fd8c1b45
Rohan <rohankumar5803@gmail.com>
2024-04-09 22:34:55 +0530

    updates about section

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 function About() {
-  return <div>
-    <div>About</div>
-    <div><p>My name is Rohan. I'm pursuing my Bachelor of Engineering degree in Computer Science and Engineering. I'm interested in the field of Web development. I have a good understanding in programming languages.</p></div>
-  </div>;
+  return (
+    <div className="flex-col text-center">
+      <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300 py-16">
+        Know me?
+      </h1>
+      <div className="flex-col">
+        <p className="w-6/12">
+          My name is Rohan. I'm pursuing my Bachelor of Engineering degree in
+          Computer Science and Engineering. I'm interested in the field of Web
+          development. I have a good understanding in programming languages.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
