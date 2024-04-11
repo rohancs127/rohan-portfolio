bf038a7aa38cd981c0f9193a8d2478bbc495e100
Rohan <rohankumar5803@gmail.com>
2024-04-11 22:14:07 +0530

    adds basic positioning

--- src/Components/SocialCard.jsx
+++ src/Components/SocialCard.jsx
@@ -3,8 +3,14 @@ import React from "react";
 function SocialCard() {
   return (
     <div>
-      <img src="../assets/socialMediaLogo/facebook.svg" />
-      <h3>username</h3>
+      <div>Contact</div>
+      <div className="flex items-center p-10 border-violet-300 shadow-sky-400">
+        <img
+          src="../../src/assets/socialMediaLogo/facebook.svg"
+          className="w-14"
+        />
+        <h3>username</h3>
+      </div>
     </div>
   );
 }
