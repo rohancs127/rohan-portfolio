cf4c5c3e5d73d91627ea35bc141975700cc1e4cf
Rohan <rohankumar5803@gmail.com>
2024-04-11 22:08:08 +0530

    creates file for social media card

---
+++ src/Components/SocialCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SocialCard() {
+  return (
+    <div>
+      <img src="../assets/socialMediaLogo/facebook.svg" />
+      <h3>username</h3>
+    </div>
+  );
+}
+
+export default SocialCard;
