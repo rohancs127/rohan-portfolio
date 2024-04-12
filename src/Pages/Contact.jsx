4f1a195201c8bfa24c48811a7bc4c4401400e6d7
Rohan <rohankumar5803@gmail.com>
2024-04-12 16:20:31 +0530

    alters positioning

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,8 +3,13 @@ import SocialCard from "../Components/SocialCard";
 
 function Contact() {
   return (
-    <div>
-      <SocialCard />
+    <div className="flex justify-center">
+      <div className="flex-col">
+        <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300">
+          Contact
+        </h1>
+        <SocialCard />
+      </div>
     </div>
   );
 }
