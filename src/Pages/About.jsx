3c72ffec5261385ac192a47175c122e0e8842284
Rohan <rohankumar5803@gmail.com>
2024-04-16 15:39:25 +0530

    fix alignment

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function About() {
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center py-44">
       <div className="flex-col text-center">
         <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300 inline-block">
           Know Me
         </h1>
-        <div className="">
+        <div className=" w-80 self-center">
           My name is Rohan. I'm pursuing my Bachelor of Engineering degree in
           Computer Science and Engineering. I'm interested in the field of Web
           development. I have a good understanding in programming languages.
