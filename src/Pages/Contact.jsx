495b85c4322725410a32fad457703e98878e17d6
Rohan <rohankumar5803@gmail.com>
2024-04-16 15:21:24 +0530

    fixes contact alignment

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,11 +3,17 @@ import SocialCard from "../Components/SocialCard";
 
 function Contact() {
   return (
-    <div className="flex justify-center">
-      <div className="flex-col">
+    <div>
+      <div className="flex justify-center p-10">
         <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300">
           Contact
         </h1>
+      </div>
+      <div className="flex flex-wrap justify-center">
+        <SocialCard />
+        <SocialCard />
+        <SocialCard />
+        <SocialCard />
         <SocialCard />
       </div>
     </div>
