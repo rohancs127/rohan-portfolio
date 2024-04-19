5c276174c3c12b89497a94a75a5621dd27a13539
Rohan <rohankumar5803@gmail.com>
2024-04-19 22:06:31 +0530

    alters anchor tag and adds links

--- src/Components/SocialCard.jsx
+++ src/Components/SocialCard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
-function SocialCard({ logo, account }) {
+function SocialCard({ logo, account, link }) {
   return (
-    <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md">
-      <img src={logo} className="w-12" />
-      <h3>{account}</h3>
-    </div>
+    <a href={link}>
+      <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md">
+        <img src={logo} className="w-12" />
+        <h3>{account}</h3>
+      </div>
+    </a>
   );
 }
 
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -10,36 +10,31 @@ function Contact() {
         </h1>
       </div>
       <div className="flex flex-wrap justify-center gap-10">
-        <a>
-          <SocialCard
-            logo="../../src/assets/socialMediaLogo/github.svg"
-            account="GitHub"
-          />
-        </a>
-        <a>
-          <SocialCard
-            logo="../../src/assets/socialMediaLogo/linked-in.svg"
-            account="LinkedIn"
-          />
-        </a>
-        <a>
-          <SocialCard
-            logo="../../src/assets/socialMediaLogo/twitterx.svg"
-            account="X"
-          />
-        </a>
-        <a>
-          <SocialCard
-            logo="../../src/assets/socialMediaLogo/facebook.svg"
-            account="Facebook"
-          />
-        </a>
-        <a>
-          <SocialCard
-            logo="../../src/assets/socialMediaLogo/instagram.svg"
-            account="Instagram"
-          />
-        </a>
+        <SocialCard
+          logo="../../src/assets/socialMediaLogo/github.svg"
+          account="GitHub"
+          link="https://github.com/rohancs127"
+        />
+        <SocialCard
+          logo="../../src/assets/socialMediaLogo/linked-in.svg"
+          account="LinkedIn"
+          link="https://www.linkedin.com/in/rohan-44aa02228"
+        />
+        <SocialCard
+          logo="../../src/assets/socialMediaLogo/twitterx.svg"
+          account="X"
+          link="https://x.com/rohanKumar5803"
+        />
+        <SocialCard
+          logo="../../src/assets/socialMediaLogo/facebook.svg"
+          account="Facebook"
+          link="https://www.facebook.com/profile.php?id=100012243734741"
+        />
+        <SocialCard
+          logo="../../src/assets/socialMediaLogo/instagram.svg"
+          account="Instagram"
+          link="https://www.instagram.com/rohan__kumar._"
+        />
       </div>
     </div>
   );
