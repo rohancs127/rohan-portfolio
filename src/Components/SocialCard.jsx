557398f461242a11eff473f788954e7e6c441242
Rohan <rohankumar5803@gmail.com>
2024-04-19 22:17:58 +0530

    changed X image size

--- src/Components/SocialCard.jsx
+++ src/Components/SocialCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SocialCard({ logo, account, link, width }) {
   return (
     <a href={link}>
-      <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md hover:shadow-purple-300 hover:shadow transition-all duration-200">
+      <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md hover:shadow-purple-300 hover:shadow transition-all duration-200 h-">
         <img src={logo} className={width} />
         <h3>{account}</h3>
       </div>
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -14,26 +14,31 @@ function Contact() {
           logo="../../src/assets/socialMediaLogo/github.svg"
           account="GitHub"
           link="https://github.com/rohancs127"
+          width="w-14"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/linked-in.svg"
           account="LinkedIn"
           link="https://www.linkedin.com/in/rohan-44aa02228"
+          width="w-14"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/twitterx.svg"
           account="X"
           link="https://x.com/rohanKumar5803"
+          width="w-10"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/facebook.svg"
           account="Facebook"
           link="https://www.facebook.com/profile.php?id=100012243734741"
+          width="w-14"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/instagram.svg"
           account="Instagram"
           link="https://www.instagram.com/rohan__kumar._"
+          width="w-14"
         />
       </div>
     </div>
