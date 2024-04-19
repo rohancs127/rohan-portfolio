87913b32b5f6353bc4be48e751c72969e97e45b5
Rohan <rohankumar5803@gmail.com>
2024-04-19 22:12:42 +0530

    added shadow and width variable

--- src/Components/SocialCard.jsx
+++ src/Components/SocialCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function SocialCard({ logo, account, link }) {
+function SocialCard({ logo, account, link, width }) {
   return (
     <a href={link}>
-      <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md">
-        <img src={logo} className="w-12" />
+      <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md hover:shadow-purple-300 hover:shadow transition-all duration-200">
+        <img src={logo} className={width} />
         <h3>{account}</h3>
       </div>
     </a>
