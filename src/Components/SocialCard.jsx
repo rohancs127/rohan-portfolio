a200fdbd44d54e3cf92f8cf40d6d431144a9501c
Rohan <rohankumar5803@gmail.com>
2024-04-24 20:13:51 +0530

    removes social media name

--- src/Components/SocialCard.jsx
+++ src/Components/SocialCard.jsx
@@ -3,9 +3,9 @@ import React from "react";
 function SocialCard({ logo, account, link, width }) {
   return (
     <a href={link}>
-      <div className="flex gap-5 items-center justify-center px-5 py-1 bg-gray-900 w-48 text-stone-100 border-purple-900 border border-opacity-50 rounded-md hover:shadow-purple-300 hover:shadow transition-all duration-200 h-">
+      <div className="flex gap-5 items-center justify-center px-3 py-3 bg-gray-900 text-stone-100 border-purple-900 border border-opacity-50 rounded-md hover:shadow-purple-300 hover:shadow transition-all duration-200">
         <img src={logo} className={width} />
-        <h3>{account}</h3>
+        {/* <h3>{account}</h3> */}
       </div>
     </a>
   );
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
     <div>
       <div className="flex justify-center p-10">
         <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300">
-          Contact
+          Contact Me
         </h1>
       </div>
       <div className="flex flex-wrap justify-center gap-10">
@@ -14,31 +14,31 @@ function Contact() {
           logo="../../src/assets/socialMediaLogo/github.svg"
           account="GitHub"
           link="https://github.com/rohancs127"
-          width="w-14"
+          width="w-12"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/linked-in.svg"
           account="LinkedIn"
           link="https://www.linkedin.com/in/rohan-44aa02228"
-          width="w-14"
+          width="w-12"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/twitterx.svg"
           account="X"
           link="https://x.com/rohanKumar5803"
-          width="w-10"
+          width="w-12"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/facebook.svg"
           account="Facebook"
           link="https://www.facebook.com/profile.php?id=100012243734741"
-          width="w-14"
+          width="w-12"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/instagram.svg"
           account="Instagram"
           link="https://www.instagram.com/rohan__kumar._"
-          width="w-14"
+          width="w-12"
         />
       </div>
     </div>
