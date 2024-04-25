130fbc74bcb92a9ff94d02da38201142c7677aa6
Rohan <rohankumar5803@gmail.com>
2024-04-25 22:04:38 +0530

    creates and imports project card

---
+++ src/Components/ProjectCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ProjectCard() {
+  return (
+    <div>
+        <img/>
+        <div>
+            <h1>Project Name</h1>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectCard
--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Project() {
   return (
-    <div>Project</div>
+    <div>
+      <div className="flex justify-center p-10">
+        <h1 className="font-bold text-indigo-300 hover:tracking-widest transition-all duration-300">
+          My Works
+        </h1>
+      </div>
+    </div>
   )
 }
 
