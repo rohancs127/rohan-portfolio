bbfa1196600bc9305519ca293b4179448cc4a591
Rohan <rohankumar5803@gmail.com>
2024-04-26 22:11:54 +0530

    improves projectcard style

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ProjectCard() {
   return (
-    <div className="flex-col items-start border border-zinc-700 p-10 w-72 rounded-lg bg-gradient-to-r from-indigo-900 to-zinc-900">
+    <div className="flex-col items-start border-none shadow shadow-sm shadow-gray-700  p-10 w-72 rounded-lg transition-all duration-300 bg-gradient-to-r from-indigo-950 to-zinc-900 hover:shadow-gray-700 hover:shadow-md">
       <h2 className="text-xl mb-7 font-bold">Project Name</h2>
-      <button className="relative left-0 right-0 bottom-0 bg-slate-50 text-indigo-950">Check out</button>
+      <button className="relative left-0 right-0 bottom-0 font-bold bg-indigo-900 text-slate-200 border-none hover:text-indigo-900 hover:bg-slate-200 transition-all duration-200">Check out</button>
     </div>
   );
 }
