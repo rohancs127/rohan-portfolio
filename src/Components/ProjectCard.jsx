cf2db4f62d4b0c6bf6ebe56d566eefac97c40562
Rohan <rohankumar5803@gmail.com>
2024-04-26 21:48:52 +0530

    adds style to projectcard

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ProjectCard() {
   return (
-    <div className="flex-col items-start border border-zinc-700 p-10 item w-72">
-      <h2 className="text-xl mb-7">Project Name</h2>
-      <button>Check out</button>
+    <div className="flex-col items-start border border-zinc-700 p-10 w-72 rounded-lg bg-indigo-950">
+      <h2 className="text-xl mb-7 font-bold">Project Name</h2>
+      <button className="relative left-0 right-0 bottom-0 bg-slate-50 text-indigo-950">Check out</button>
     </div>
   );
 }
