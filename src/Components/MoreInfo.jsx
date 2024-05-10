b28b720bd22f69da542e0a93653a77ea2e43279a
Rohan <rohankumar5803@gmail.com>
2024-05-10 22:00:05 +0530

    adss education list to more info

--- src/Components/MoreInfo.jsx
+++ src/Components/MoreInfo.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 function MoreInfo() {
   return (
-    <div></div>
+    <div>
+        <div>
+            <h2>Education</h2>
+            <div>
+                <div>Highschool: Sri Ramakrishna Highschool Puttur</div>
+                <div>2016-2019</div>
+            </div>
+            <div>
+                <div>Pre-University: St. Philomena PU College Puttur</div>
+                <div>2019-2021</div>
+            </div>
+            <div>
+                <div>Graduation: Sahyadri College Of Engineering & Management</div>
+                <div>Computer Science & Engineering</div>
+                <div>2021-Present</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
