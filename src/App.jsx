cd72354b8eaba2f763004673e1fd37f0ed96c676
Rohan <rohankumar5803@gmail.com>
2024-05-19 20:17:57 +0530

    creates refs for scrol navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Home from './Pages/Home';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import { useRef } from "react";
 
 function App() {
+  const homeRef = useRef(null);
+  const skillRef = useRef(null);
+  const aboutRef = useRef(null);
+  const projectRef = useRef(null);
+  const contactRef = useRef(null);
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <Navbar
+        homeRef={homeRef}
+        skillRef={skillRef}
+        aboutRef={aboutRef}
+        projectRef={projectRef}
+        contactRef={contactRef}
+      />
+      <Home
+        homeRef={homeRef}
+        skillRef={skillRef}
+        aboutRef={aboutRef}
+        projectRef={projectRef}
+        contactRef={contactRef}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
