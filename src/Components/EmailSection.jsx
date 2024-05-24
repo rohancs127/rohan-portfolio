c6688f8a3750033afaae75597ec1b27801d3205c
Rohan <rohankumar5803@gmail.com>
2024-05-24 21:13:43 +0530

    adds style and replaces input with textarea

--- src/Components/EmailSection.jsx
+++ src/Components/EmailSection.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 function EmailSection() {
   return (
-    <div className="flex-col">
-      <div className="self-center">Mail me</div>
-      <div><input type="email" placeholder="Enter email address" /></div>
-      <div className="flex-col">
-        <div><input placeholder="Enter message" /></div>
-        <div><button>Send</button></div>
+    <div className="flex-col text-center">
+      <div className="mt-5">Mail me</div>
+      <div className="mt-5">
+        <input type="email" placeholder="Enter email address" />
+      </div>
+      <div className="flex-col mt-5">
+        <div>
+          <textarea placeholder="Enter message" className="px-3 pr-6 resize-none" />
+        </div>
+        <div className="mt-5">
+          <button>Send</button>
+        </div>
       </div>
     </div>
   );
