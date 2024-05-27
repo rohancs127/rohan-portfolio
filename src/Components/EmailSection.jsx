372a8d0751aefd7c0326f59d9f4bfb705e5086ae
Rohan <rohankumar5803@gmail.com>
2024-05-27 20:59:10 +0530

    styles email address section

--- src/Components/EmailSection.jsx
+++ src/Components/EmailSection.jsx
@@ -5,11 +5,11 @@ function EmailSection() {
     <div className="flex-col text-center">
       <div className="mt-5 text-lg">Mail me</div>
       <div className="mt-5">
-        <input type="email" placeholder="Enter email address" />
+        <input type="email" placeholder="Enter email address" className="outline-none px-3 py-1 rounded-lg"/>
       </div>
       <div className="flex-col mt-5">
         <div>
-          <textarea placeholder="Enter message" className="px-3 pr-6 resize-none" />
+          <textarea placeholder="Enter message" className="px-3 pr-16 resize-none outline-none overflow-hidden rounded-lg" />
         </div>
         <div className="mt-5">
           <button className=" focus:outline-none relative left-0 right-0 bottom-0 font-bold bg-indigo-900 text-slate-200 border-none shadow hover:shadow-sm hover:shadow-indigo-400 transition-all duration-200 mb-3">Send</button>
