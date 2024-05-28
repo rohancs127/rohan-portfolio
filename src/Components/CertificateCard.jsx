26d2da5f52bc9c479424fd168eed37bc36cd4f6e
Rohan <rohankumar5803@gmail.com>
2024-05-28 21:25:02 +0530

    creates certificates section

---
+++ src/Components/CertificateCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CertificateCard() {
+  return (
+    <div>
+        <div>{content}</div>
+    </div>
+  )
+}
+
+export default CertificateCard
---
+++ src/Components/CertificatesSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CertificatesSection() {
+  return (
+    <div></div>
+  )
+}
+
+export default CertificatesSection
