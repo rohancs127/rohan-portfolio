630de1f8e976df8ece3487e27552c02e08a57c9d
Rohan <rohankumar5803@gmail.com>
2024-05-31 21:43:52 +0530

    styles education button

--- src/Components/MoreInfoOption.jsx
+++ src/Components/MoreInfoOption.jsx
@@ -3,10 +3,10 @@ import { ChevronDown } from "lucide-react";
 
 function MoreInfoOption() {
   return (
-    <div>
+    <button className="flex items-center bg-gradient-to-r from-blue-800 to-indigo-900 focus:outline-none border-none">
       <h2 className="text-xl">Education</h2>
       <ChevronDown />
-    </div>
+    </button>
   );
 }
 
