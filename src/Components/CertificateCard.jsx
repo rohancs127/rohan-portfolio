edff893d9f8916af915295918782ff83e7770300
Rohan <rohankumar5803@gmail.com>
2024-07-15 22:54:58 +0530

    sets up certificates div

--- src/Components/CertificateCard.jsx
+++ src/Components/CertificateCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function CertificateCard() {
   return (
-    <div className="">
+    <div className="h-52">
       <img src="images/demo-certificate.jpeg" className="h-52"/>
-      <div className="relative bottom-52 text-gray-100 bg-indigo-400 bg w-full h-full flex-col">
-        <h1 className="">Certificate</h1>
+      <div className=" opacity-0 hover:opacity-80 duration-200 relative bottom-52 text-gray-100 bg-indigo-400 bg w-full h-full flex-col pt-10 transform">
+        <h1 className="text-lg font-bold">Certificate</h1>
       </div>
     </div>
   );
