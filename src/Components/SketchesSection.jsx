d4511313937b58ed4229146f7a253b8999ee2848
Rohan <rohankumar5803@gmail.com>
2024-07-18 21:39:52 +0530

    creates sketches section file

---
+++ src/Components/SketchesSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SketchCard from "./SketchCard";
+
+function SketchesSection() {
+  return (
+    <div>
+      <SketchCard />
+    </div>
+  );
+}
+
+export default SketchesSection;
