935bb3779ff66bc9ea0923ef2172ea41c6791f7b
Rohan <rohankumar5803@gmail.com>
2024-07-19 22:09:56 +0530

    adds horizontal scroll option  for sketches

--- src/Components/SketchesSection.jsx
+++ src/Components/SketchesSection.jsx
@@ -3,13 +3,19 @@ import SketchCard from "./SketchCard";
 
 function SketchesSection() {
   return (
-    <div className="grid grid-flow-col gap-10 items-center justify-center m-20">
+    <div className="grid grid-flow-col gap-10 items-center justify-center m-20 overflow-x-scroll overflow-y-hidden scrollbar-hide">
       <SketchCard />
       <SketchCard />
       <SketchCard />
       <SketchCard />
       <SketchCard />
-
+      <SketchCard />
+      <SketchCard />
+      <SketchCard />
+      <SketchCard />
+      <SketchCard />
+      <SketchCard />
+      <SketchCard />
     </div>
   );
 }
