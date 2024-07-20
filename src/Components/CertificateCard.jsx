c9b21b70a72bba2f3360ee30d00a36dfa1cc878c
Rohan <rohankumar5803@gmail.com>
2024-07-20 21:38:11 +0530

    adds scroll feature for certificate section

--- src/Components/CertificateCard.jsx
+++ src/Components/CertificateCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function CertificateCard() {
   return (
-    <div className="h-52 rounded-xl overflow-hidden">
-      <img src="images/demo-certificate.jpeg" className="h-52"/>
+    <div className="h-52 w-72 rounded-xl overflow-hidden">
+      <img src="images/demo-certificate.jpeg" className="h-52" />
       <div className=" opacity-0 hover:opacity-80 duration-200 relative bottom-52 text-gray-100 bg-indigo-400 bg w-full h-full flex-col pt-10 transform">
         <h1 className="text-lg font-bold">Certificate</h1>
       </div>
--- src/Components/CertificatesSection.jsx
+++ src/Components/CertificatesSection.jsx
@@ -3,7 +3,12 @@ import CertificateCard from "./CertificateCard";
 
 function CertificatesSection() {
   return (
-    <div className="grid grid-flow-col gap-10 justify-center mb-32">
+    <div className="grid grid-flow-col gap-10 items-center justify-center m-20 overflow-x-scroll overflow-y-hidden scrollbar-hide pl-72">
+      <CertificateCard />
+      <CertificateCard />
+      <CertificateCard />
+      <CertificateCard />
+      <CertificateCard />
       <CertificateCard />
       <CertificateCard />
       <CertificateCard />
