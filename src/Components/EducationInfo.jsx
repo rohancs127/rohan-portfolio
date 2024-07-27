15dd509387353944d8f2968cda358e86d83cd0dc
Rohan <rohankumar5803@gmail.com>
2024-07-27 22:35:09 +0530

    change bg color of education info

--- src/Components/EducationInfo.jsx
+++ src/Components/EducationInfo.jsx
@@ -4,15 +4,15 @@ function EducationInfo() {
   return (
     <div>
       <div className="text-lg flex-col text-center mx-72 rounded-lg overflow-hidden mb-5 shadow shadow-md shadow-zinc-800">
-        <div className="bg-slate-900 p-5">
+        <div className="bg-gradient-to-r from-indigo-950 to-zinc-900 p-5">
           <div>Highschool: Sri Ramakrishna Highschool Puttur</div>
           <div>2016-2019</div>
         </div>
-        <div className="bg-slate-900 p-5">
+        <div className="bg-gradient-to-r from-indigo-950 to-zinc-900 p-5">
           <div>Pre-University: St. Philomena PU College Puttur</div>
           <div>2019-2021</div>
         </div>
-        <div className="bg-slate-900 p-5">
+        <div className="bg-gradient-to-r from-indigo-950 to-zinc-900 p-5">
           <div>Graduation: Sahyadri College Of Engineering & Management</div>
           <div>Computer Science & Engineering</div>
           <div>2021-Present</div>
