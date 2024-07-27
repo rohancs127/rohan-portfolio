60b1c285214c30684d90aadcda22f4046ff1547d
Rohan <rohankumar5803@gmail.com>
2024-07-27 21:59:58 +0530

    add sketches to page

--- src/Components/SketchCard.jsx
+++ src/Components/SketchCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function SketchCard() {
+function SketchCard({sketch}) {
   return (
-    <div className=" bg-slate-600 h-52 w-52 flex-shrink-0">
-      <img />
+    <div className="bg-black h-52 w-52 flex-shrink-0 hover:shadow-xl hover:shadow-slate-200 flex justify-center transition-all duration-300">
+      <img src={sketch}  className="w-48 h-52"/>
     </div>
   );
 }
--- src/Components/SketchesSection.jsx
+++ src/Components/SketchesSection.jsx
@@ -3,19 +3,14 @@ import SketchCard from "./SketchCard";
 
 function SketchesSection() {
   return (
-    <div className="flex gap-10 items-center justify-start my-20  overflow-x-auto scrollbar-hide px-20">
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
-      <SketchCard />
+    <div className="flex justify-center gap-10 items-center my-20  overflow-x-auto overflow-y-hidden scrollbar-hide px-20">
+      <SketchCard sketch="src/assets/Sketches/Sketch1.jpg" />
+      <SketchCard sketch="src/assets/Sketches/Sketch2.jpeg" />
+      <SketchCard sketch="src/assets/Sketches/Sketch3.jpeg" />
+      <SketchCard sketch="src/assets/Sketches/Sketch4.jpeg" />
+      <SketchCard sketch="src/assets/Sketches/Sketch5.jpeg" />
+      <SketchCard sketch="src/assets/Sketches/Sketch6.jpeg" />
+
     </div>
   );
 }
