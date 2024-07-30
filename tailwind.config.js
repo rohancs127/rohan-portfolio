ee29298eb7572be25e587328d3f988e09675c8c6
Rohan <rohankumar5803@gmail.com>
2024-07-30 21:38:15 +0530

    setup animation extension

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,40 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      padding:{
-        '128': '32rem',
+      padding: {
+        128: "32rem",
       },
-      userSelect:{
-        none : 'none'
-      }
+      userSelect: {
+        none: "none",
+      },
+      keyframes: {
+        "slide-in-right": {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-in-left": {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-in-up": {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "slide-in-down": {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "slide-in-right": "slide-in-right 0.5s ease-out forwards",
+        "slide-in-left": "slide-in-left 0.5s ease-out forwards",
+        "slide-in-up": "slide-in-up 0.3s ease-out forwards",
+        "slide-in-down": "slide-in-down 0.5s ease-out forwards",
+      },
     },
   },
   variants: {
-    userSelect: ['responsive', 'focus'],
+    userSelect: ["responsive", "focus"],
   },
   plugins: [require("tailwind-scrollbar-hide")],
 };
