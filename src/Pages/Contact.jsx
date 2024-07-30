4a792c0dd956cfe412ef3ccdd2822595331fa920
Rohan <rohankumar5803@gmail.com>
2024-07-30 20:08:10 +0530

    add source code link

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -13,37 +13,41 @@ function Contact() {
       <div className="flex justify-center">
         <EmailSection />
       </div>
-      <div className="flex flex-wrap justify-center gap-10 mt-10">
+      <div className="flex flex-wrap justify-center gap-10 mt-10 max-sm:gap-5">
         <SocialCard
           logo="../../src/assets/socialMediaLogo/github.svg"
           account="GitHub"
           link="https://github.com/rohancs127"
-          width="w-12"
+          width="w-12 max-sm:w-7"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/linked-in.svg"
           account="LinkedIn"
           link="https://www.linkedin.com/in/rohan-44aa02228"
-          width="w-12"
+          width="w-12 max-sm:w-7"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/twitterx.svg"
           account="X"
           link="https://x.com/rohanKumar5803"
-          width="w-12"
+          width="w-12 max-sm:w-7"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/facebook.svg"
           account="Facebook"
           link="https://www.facebook.com/profile.php?id=100012243734741"
-          width="w-12"
+          width="w-12 max-sm:w-7"
         />
         <SocialCard
           logo="../../src/assets/socialMediaLogo/instagram.svg"
           account="Instagram"
           link="https://www.instagram.com/rohan__kumar._"
-          width="w-12"
+          width="w-12 max-sm:w-7"
         />
+      </div>
+      <div className="text-center mt-20">
+        <div>Source code available at</div>
+        <a href="https://github.com/rohancs127/rohan-portfolio.git"><div className="flex items-center justify-center text-lg text-white cursor-pointer m-5"><img src="src/assets/socialMediaLogo/github.svg" className="w-8"/>/rohancs127</div></a>
       </div>
     </div>
   );
