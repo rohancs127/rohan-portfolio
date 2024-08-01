424e688828edd91fee30f5224ab0a3c3ae22ce48
Rohan <rohankumar5803@gmail.com>
2024-08-01 20:45:27 +0530

    add animation for about section content

--- src/Components/EducationInfo.jsx
+++ src/Components/EducationInfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function EducationInfo() {
   return (
     <div>
-      <div className="text-lg flex-col text-center mx-72 rounded-lg overflow-hidden mb-5 shadow shadow-md shadow-zinc-800 max-sm:m-10">
+      <div className="text-lg flex-col text-center mx-72 rounded-lg overflow-hidden mb-5 shadow shadow-md shadow-zinc-800 max-sm:m-10 animate-slide-in-up">
         <div className="bg-gradient-to-r from-indigo-950 to-zinc-900 p-5">
           <div>Highschool: Sri Ramakrishna Highschool Puttur</div>
           <div>2016-2019</div>
