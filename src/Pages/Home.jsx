95e43427948b82f67bb2803f03bfa0cd77152439
Rohan <rohankumar5803@gmail.com>
2024-08-01 21:46:43 +0530

    add jsx file for typewriterupdate html head file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,20 +8,20 @@ function Home({ homeRef, skillRef, aboutRef, projectRef, contactRef }) {
   return (
     <div className="max-sm:select-none">
       <div
-        className="flex w-full pt-60 mb-32 mr-24 py-32 justify-center gap-60 items-center text-indigo-200 max-sm:gap-20 max-sm:pt-40 max-sm:flex-col max-sm:items-center"
+        className="flex w-full pt-60 mb-32 mr-24 py-32 justify-center gap-60 items-center text-indigo-200 max-sm:gap-10 max-sm:pt-28 max-sm:flex-col max-sm:items-center"
         ref={homeRef}
       >
         <div>
           <img
             src="src/assets/Profile/Rohan.jpg"
             width={300}
-            className="rounded-full animate-slide-in-left"
+            className="rounded-full animate-slide-in-left max-sm:w-56"
           />
         </div>
         <div className="flex-col pt-10 max-sm:ml-10 animate-slide-in-down">
-          <h1 className="mb-5 font-bold text-violet-400 text-7xl p-5">Hi</h1>
-          <h2 className="mb-5 font-bold text-5xl p-5">I am Rohan</h2>
-          <h3 className="text-4xl font-mono p-5 ">
+          <h1 className="mb-5 font-bold text-violet-400 text-7xl p-5 max-sm:text-6xl max-sm:py-1">Hi</h1>
+          <h2 className="mb-5 font-bold text-5xl p-5 max-sm:text-4xl max-sm:py-1">I am Rohan</h2>
+          <h3 className="text-4xl font-mono p-5 max-sm:text-3xl max-sm:py-1">
             I'm a Computer Science Engineer..
           </h3>
         </div>
