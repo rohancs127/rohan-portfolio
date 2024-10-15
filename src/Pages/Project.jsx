69d6a9f305441b7f0d1e31edefb9e85dc590f0a6
Rohan <rohankumar5803@gmail.com>
2024-10-15 21:14:27 +0530

    add bus reservation app to project list

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -10,6 +10,10 @@ function Project() {
         </h1>
       </div>
       <div className="flex gap-20 justify-center mt-14 max-sm:flex-col max-sm:items-center">
+      <ProjectCard
+          projectName="Bus Reservation App"
+          projectLink="https://github.com/rohancs127/Bus-Reservation-App"
+        />
         <ProjectCard
           projectName="Deepfake Voice Detection"
           projectLink="https://github.com/rohancs127/Deepfake_voice_detection"
